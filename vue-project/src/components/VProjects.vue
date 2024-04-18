a6822e2250c2e9e6f2792738fdd1765dd10146fc
Danila Shafikov <IceCubeWithTea@yandex.ru>
2024-04-19 00:07:30 +0500

    Перенес код на Vue

---
+++ vue-project/src/components/VProjects.vue
@@ -0,0 +1,39 @@
+<script>
+</script>
+
+<template>
+  <section class="projects">
+    <div class="container">
+      <h2>
+        Проекты
+      </h2>
+      <div class="row">
+        <div class="col-sm-6 col-lg-4">
+          <div class="projects__cards">
+            <img src="/edukesha.png" alt="">
+            <a href="#">EduКеша</a>
+            <p>Разработали сервис для проведения онлайн-занятий </p>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="projects__cards">
+            <img src="/donatelike.png" alt="">
+            <a href="#">DonateLike</a>
+            <p>Разработали телеграмм-бота для отправки платных реакций</p>
+          </div>
+        </div>
+        <div class="col-sm-6 mb-4 col-lg-4">
+          <div class="projects__cards">
+            <img src="/surgu.png" alt="">
+            <a href="#">go.surgu.ru</a>
+            <p>Разработали сайт приемной комиссии Сургутского государственного университета</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/components/VTech.vue
@@ -0,0 +1,66 @@
+<template>
+    <section class="tech">
+      <div class="container">
+        <h2>
+          Технологии
+        </h2>
+        <div class="row">
+          <div class="col-6 col-md-4 col-lg-3">
+            <div class="tech__cards">
+              <img src="/php.png" alt="">
+              <div class="tech__cards-name">PHP</div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4 col-lg-3">
+            <div class="tech__cards">
+              <img src="/laravel.png" alt="">
+              <div class="tech__cards-name">Laravel</div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4 col-lg-3">
+            <div class="tech__cards">
+              <img src="/js.png" alt="">
+              <div class="tech__cards-name">JavaScript</div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4 col-lg-3">
+            <div class="tech__cards">
+              <img src="/nuxt.png" alt="">
+              <div class="tech__cards-name">Nuxt.js</div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4 col-lg-3">
+            <div class="tech__cards">
+              <img src="/mySQL.png" alt="">
+              <div class="tech__cards-name">MySQL</div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4 col-lg-3">
+            <div class="tech__cards">
+              <img src="/postgreSQL.png" alt="">
+              <div class="tech__cards-name">PostgreSQL</div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4 col-lg-3">
+            <div class="tech__cards">
+              <img src="/api.png" alt="">
+              <div class="tech__cards-name">RESTful API</div>
+            </div>
+          </div>
+          <div class="col-6 col-md-4 col-lg-3">
+            <div class="tech__cards">
+              <img src="/gitlab.png" alt="">
+              <div class="tech__cards-name">GitLab</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
